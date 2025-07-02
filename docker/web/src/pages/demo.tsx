0aec5f228bbad401ae5fa26e9e52ed401c4aef36
yori <yorirugby13@gmail.com>
2025-07-02 21:01:42 +0900

    Topページへのページ遷移を実装

--- docker/web/src/pages/demo.tsx
+++ docker/web/src/pages/demo.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import DemoComponent from '../components/demoComponent';
 
 export default function DemoPage(){
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/top');
+  }
   return(
     <>
       <h1>demoPage!!</h1>
       <DemoComponent/>
+      <button onClick={handleClick}>押してくれ</button>
     </>
   );
 }
