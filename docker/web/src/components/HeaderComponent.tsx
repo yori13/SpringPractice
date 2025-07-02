5f94d8b995035ef6514e1a0aee5edcc6434c277c
yori <yorirugby13@gmail.com>
2025-07-02 20:45:32 +0900

    ヘッダーのコンポーネント作成

---
+++ docker/web/src/components/HeaderComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HeaderComponent() {
+  return(
+    <>
+      <div>
+        <nav>
+          <h1>めもアプリ</h1>
+        </nav>
+      </div>
+    </>
+  );
+}
