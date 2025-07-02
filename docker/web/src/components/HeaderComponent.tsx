6e3eb75d5d72b65731fb6bf247a04885ad050e8a
yori <yorirugby13@gmail.com>
2025-07-02 21:33:26 +0900

    ヘッダー作成

--- docker/web/src/components/HeaderComponent.tsx
+++ docker/web/src/components/HeaderComponent.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function HeaderComponent() {
+  const handleLogout = () => {
+    alert('チョコミントよりもあ・な・た❤️');
+  }
   return(
     <>
-      <div>
-        <nav>
-          <h1>めもアプリ</h1>
-        </nav>
-      </div>
+      <nav className="navbar d-flex justify-content-between align-items-center px-3" style={{ backgroundColor: '#5624d0' }}>
+        <h1 className="text-white m-0">めもアプリ</h1>
+        <p className="text-white m-0" style={{ cursor: 'pointer' }} onClick={handleLogout} >
+          ログアウト
+        </p>
+      </nav>
     </>
   );
 }
