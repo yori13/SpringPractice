d256cf86da966c439afa9f6434fa17dcffad4e72
yori <yorirugby13@gmail.com>
2025-07-02 20:45:21 +0900

    トップページ作成

---
+++ docker/web/src/pages/TopPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HeaderComponent from "../components/HeaderComponent";
+
+export default function TopPage() {
+  return(
+    <>
+      <HeaderComponent/>
+    </>
+  );
+}
