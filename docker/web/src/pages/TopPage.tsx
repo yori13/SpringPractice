5235b1b7a70ca24b3c859203cbf377be30e646a1
yori <yorirugby13@gmail.com>
2025-07-02 22:31:57 +0900

    タブの実装

--- docker/web/src/pages/TopPage.tsx
+++ docker/web/src/pages/TopPage.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderComponent from "../components/HeaderComponent";
+import TaskListComponent from "../components/TaskListComponent";
+import AddTaskComponent from "../components/AddTaskComponent";
 
 export default function TopPage() {
+  // activTab変数を管理するusestate
+  // 初期値としてtaskを持たせている
+  const [activeTab, setActiveTab] = useState('task');
   return(
     <>
       <HeaderComponent/>
+      <div className="row">
+        {/* onClickイベントで持たせている引数がactiveTabに渡している */}
+        <div
+          className="tab-pane col-6 border p-3"
+          style={{ backgroundColor: activeTab === 'task' ? '#FFD700' : '#DDDDDD' }}
+          onClick={() => setActiveTab('task')}
+        >
+          <h2 className="text-center">一覧</h2>
+        </div>
+        <div
+          className="tab-pane col-6 border p-3"
+          style={{ backgroundColor: activeTab === 'add' ? '#FFD700' : '#DDDDDD' }}
+          onClick={() => setActiveTab('add')}
+        >
+          <h2 className="text-center">追加</h2>
+        </div>
+        <div>
+          {activeTab === 'task' && (
+            <div id="task">
+              <TaskListComponent/>
+            </div>
+          )}
+          {activeTab === 'add' && (
+            <div id="add">
+              <AddTaskComponent/>
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
 }
